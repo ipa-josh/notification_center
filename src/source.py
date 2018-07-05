e373c60187071e9de28bf3bb02998c3ea16ecfa9
Joshua Hampp <j.hampp@denso-adas.de>
2018-07-05 07:42:44 +0200

    source class

---
+++ src/source.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+class Source:
+	
+	def __init__(self, name, config):
+		self.name = name
+		self.config = config
+		self.subscribers = []
+		
+	def subscribe(self, s):
+		self.subscribers.append(s)
+		
+	def notify(self):
+		for s in self.subscribers:
+			s.notify(self)
+			
+	def name(self):
+		return self.name
+		
+	def name_nice(self):
+		return self.name()
+		
+	def summary(self):
+		return ""
+		
+	def text(self):
+		return ""
+		
+	def priority(self):
+		return 0
